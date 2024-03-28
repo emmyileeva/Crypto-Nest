788fe866b3fd9365778a9d52763079fa9a43039d
Emmy <emilyileeva@gmail.com>
2024-03-28 15:23:52 -0700

    basic setup for landing.css

--- Crypto-Nest/routes/trades.js
+++ Crypto-Nest/routes/trades.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const ensureLoggedIn = require("../config/ensureLoggedIn");
+
 // Route to render trade.ejs
 router.get("/", function (req, res) {
   res.render("trade"); 
